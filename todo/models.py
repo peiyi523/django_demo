08704447c457eea42847e95b5549dadaeb98e794
peiyi <peiyi523@gmail.com>
2024-09-28 15:34:07 +0800

    完成todo資料表建立“

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+class Todo(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.TextField(blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    date_complated = models.DateTimeField(blank=True, null=True)
+    important = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.id}-{self.title}-{self.created}"

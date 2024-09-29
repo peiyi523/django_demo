de0f3ae7d0d359721471c6aa7b3659eae75ec9f7
peiyi <peiyi523@gmail.com>
2024-09-29 17:10:35 +0800

    1.除錯

--- todo/models.py
+++ todo/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 # Create your models here.
@@ -8,6 +9,7 @@ class Todo(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     date_complated = models.DateTimeField(blank=True, null=True)
     important = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.id}-{self.title}-{self.created}"
